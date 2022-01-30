d4c3f2deed74f9dc3f358320a0afe91e78d91ec0
dankarger <karger.dan@gmail.com>
2022-01-30 20:40:06 +0200

    working on SearchForm, Home, Button.css, InputFields

--- client/src/Utils/InputField.jsx
+++ client/src/Utils/InputField.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import './InputField.css'
+
 
 export default function InputField(type, name, placeholder, value, onChange,icon) {
 
 
     return(
-        <div>
-            <label htmlFor={name}>{name} {icon}</label>
-            {/*<p>חפש שם</p>*/}
-            <input name={name} type={type} placeholder={placeholder} value={value} onChange={onChange}/>
+        <div className='input-div'>
+            {/*<label htmlFor={name}>{name} </label>*/}
+            <p>חפש שם</p>
+            <input className="input" name={name} type={type} placeholder={placeholder} value={value} onChange={onChange}/>
         </div>
     )
 }
