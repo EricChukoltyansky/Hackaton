f838da30a08e84ca27928272ebfb445c18fa4ee8
sarit-chaet-hudis <sarit.chh@gmail.com>
2022-01-30 16:06:56 +0200

    results page initial

--- client/src/Styles/Card.style.js
+++ client/src/Styles/Card.style.js
@@ -1,10 +1,13 @@
 // import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from "react/cjs/react.production.min";
 import styled from "styled-components";
 
-const CardWrapper = styled.div`
+export const CardStyle = styled.div`
   border: 1px solid red;
   padding: 20px;
   margin: 10px;
 `;
 
-module.exports = { CardWrapper };
+export const CardDetails = styled.div`
+  display: flex;
+  align-items: stretch;
+`;
