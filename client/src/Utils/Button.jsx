678c16ad90f2eacb20524bf2ab4e292974b97f8f
dankarger <karger.dan@gmail.com>
2022-01-30 14:47:59 +0200

    change App.js add Button.styled.js add HomePage.styled.js change index.js

--- client/src/Utils/Button.jsx
+++ client/src/Utils/Button.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-
+import {ButtonStyled} from "../Styles/Button.style";
 
 export default function Button({ name,callback,icon,className,disabled ,onMouseOver}) {
+
+
+
     return(
         <>
-            <button className={className} onClick={callback} disabled={!!disabled} onMouseOver={onMouseOver}> {name}  <span>{icon}</span></button>
+            <ButtonStyled>
+                <button className={className} onClick={callback} disabled={!!disabled} onMouseOver={onMouseOver}> {name}  <span>{icon}</span></button>
+            </ButtonStyled>
         </>
     )
 }
